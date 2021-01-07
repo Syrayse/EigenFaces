5d9837dfca641fc31d1cc07b05d6b9067e2d1d56
Gonçalo Almeida <48161282+Gonkalos@users.noreply.github.com>
2021-01-07 17:49:03 +0000

    add predict occlusion and goncalo's custom photos

--- src/EigenSpace.py
+++ src/EigenSpace.py
@@ -110,12 +110,15 @@ class EigenSpace:
         # Select closest eigen face
         d_min = np.min(dist)
 
+        # get image id
+        index = dist.index(d_min)
+
         if d_min > threshold:
             tagPred = None
         else:
             tagPred = self.tags[np.argmin(dist)]
 
-        return tagPred, d_min
+        return tagPred, d_min, index
 
     def plotEigenValues(self, pltWidth = 10, pltHeight = 10):
         plt.figure(figsize=(pltWidth,pltHeight))
@@ -136,3 +139,71 @@ class EigenSpace:
             eigenData = self.eigenFaces.T[idxFace]
             width, height = self.images[idxFace].size
             plt.matshow(np.reshape(eigenData, (height, width)), cmap='gray')
+
+    # Predict occluded part of a face 
+    def predictOcclusion(self, imgFile, left, top, right, bottom, factor):
+        # predict person
+        occludedImage = Image.open(f'{imgFile}').convert('L')
+        occludedImageData = occludedImage.getdata()
+        personId, distance, index = self.predictFace(occludedImageData)
+        # get indexes of the person's data
+        indexes = [i for i, x in enumerate(self.tags) if x == personId]
+        # get the mean eigenface of the person
+        data = np.take(self.imgData, indexes, 0)
+        avgFace = np.mean(np.array(data),0)
+        # get image dimension 
+        width, height = self.images[0].size
+        # show person's mean eigenface
+        #plt.matshow(np.reshape(avgFace, (height, width)), cmap='gray')
+        # reconstruct face
+        reconstructedFace = self.reconstructFace(index)
+        # turn reconstructed face into an image
+        data = np.reshape(reconstructedFace, (height, width))
+        reconstructedImage = Image.fromarray(data)
+        # adjust reconstructed image brightness
+        #enhancer = ImageEnhance.Brightness(reconstructedImage)
+        #newFace = self.replacePixels(occludedImage, reconstructedImageA, left, top, right, bottom)
+        #reconstructedImageA = enhancer.enhance(factor)
+        im2 = reconstructedImage.point(lambda p: p * factor)
+        newFace = self.replacePixels(occludedImage, im2, left, top, right, bottom)
+        # replace occlusion
+        #newFace = self.replacePixels(occludedImage, reconstructedImage, left, top, right, bottom)
+        # show images
+        plt.matshow(np.reshape(occludedImage, (height, width)), cmap='gray')
+        plt.title('Occluded Image')
+        self.plotEigenFace(index)
+        plt.title('Closest Eigenface')
+        plt.matshow(np.reshape(reconstructedImage, (height, width)), cmap='gray')
+        plt.title('Closest Eigenface Reconstructed')
+        plt.matshow(np.reshape(newFace, (height, width)), cmap='gray')
+        plt.title('Oclusion Prediction')
+
+    # Reconstruct image by dotting the face’s weights with each eigenface
+    def reconstructFace(self, imageId):
+        # obtain the weights by dotting the mean centered data with the eigenfaces
+        weights = np.dot(self.imgData, self.eigenFaces)
+        # dot the face’s weights with the transpose of the eigenfaces and add the mean face back in
+        reconstructed = self.avgFace + np.dot(weights[imageId, :], self.eigenFaces.T)
+        return reconstructed
+
+    # Reconstruct image using only k eigenfaces
+    def reconstructFaceK(self, imageId, k):
+        # obtain the weights by dotting the mean centered data with the eigenfaces
+        weights = np.dot(self.imgData, self.eigenFaces)
+        # dot the face’s weights with the transpose of the eigenfaces and add the mean face back in
+        reconstructed  = self.avgFace + np.dot(weights[imageId, 0:k], self.eigenFaces[:, 0:k].T)
+        return reconstructed
+
+    # Replace part of an image with part of another image
+    def replacePixels(self, occludedImage, reconstructedImage, left, top, right, bottom):
+        replaceImg = reconstructedImage.crop((left, top, right, bottom))
+        newFace = occludedImage.copy()
+        newFace.paste(replaceImg, (left, top))
+        return newFace
+
+    # Create occluded image
+    def createOcclusion(self, sourceImagePath, destination, left, top, right, bottom):
+        face = Image.open(f'{sourceImagePath}').convert('L')
+        black = Image.open(f'../images/black.png').convert('L')
+        newImage = self.replacePixels(face, black, left, top, right, bottom)
+        newImage.save(destination)
